e11ec3853281ab0f2df797e469aa02baf0e51425
Pankaj Kumar <65084148+pkrhce@users.noreply.github.com>
2026-09-05 15:49:02 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" # Use the 5.x provider
+    }
+  }
+}
